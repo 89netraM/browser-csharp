5d914b92f543da39e16804b06537b047db63de26
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-13 20:20:46 +0200

    Loading reference dependencies at runtime

--- wasm/Program.cs
+++ wasm/Program.cs
@@ -1,14 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.CodeAnalysis;
 
 namespace BrowserCSharp
 {
-	public class Program
+	public static class Program
 	{
-		public static void Main(string[] args)
+		private const string frameworkBinUri = "_framework/_bin";
+		private static readonly IImmutableSet<string> references = ImmutableHashSet.Create(
+			"mscorlib",
+			"netstandard",
+			"System"
+		);
+
+		private static Task<PortableExecutableReference[]> loadedReferences;
+
+		public static void Main()
 		{
-			WebAssemblyHostBuilder.CreateDefault(args);
-			Console.WriteLine("Hello from C#");
+			WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault();
+			loadedReferences = GetReferences(builder.HostEnvironment.BaseAddress);
+		}
+
+		private static Task<PortableExecutableReference[]> GetReferences(string baseUri)
+		{
+			static PortableExecutableReference toReference(Task<Stream> completedTask)
+			{
+				if (completedTask.IsCompletedSuccessfully)
+				{
+					return MetadataReference.CreateFromStream(completedTask.Result);
+				}
+				else
+				{
+					throw new Exception("Could not load a reference required for runtime compilation.", completedTask.Exception);
+				}
+			}
+
+			Assembly[] loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+			using HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri(baseUri);
+
+			IDictionary<string, Task<PortableExecutableReference>> foundReferences = new Dictionary<string, Task<PortableExecutableReference>>();
+
+			foreach (Assembly assembly in loadedAssemblies)
+			{
+				string name = assembly.GetName().Name;
+				if (references.Contains(name) && !foundReferences.ContainsKey(name))
+				{
+					Task<PortableExecutableReference> task = client.GetStreamAsync(Path.Join(frameworkBinUri, assembly.Location)).ContinueWith(toReference);
+					foundReferences.Add(name, task);
+				}
+			}
+
+			if (references.All(foundReferences.ContainsKey))
+			{
+				return Task.WhenAll(foundReferences.Values);
+			}
+			else
+			{
+				return Task.FromException<PortableExecutableReference[]>(
+					new Exception("Could not find all required references for runtime compilation. " +
+						$"Missing references: {String.Join(", ", references.Except(foundReferences.Keys))}")
+				);
+			}
 		}
 	}
 }
