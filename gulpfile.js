c66e4b4a0230a8a35476cbf8f06f07a5adf9c35a
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-14 13:53:34 +0200

    Init the TypeScript project

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const del = require("del");
-const { series, src, dest } = require("gulp");
+const { series, src, dest, parallel } = require("gulp");
 const { exec } = require("child_process");
+const path = require("path");
+const { createProject } = require("gulp-typescript");
+const merge = require("merge2");
 
 const wasmProj = "./wasm/BrowserCSharp.csproj";
 const wasmSrc = "./wasm/bin/Release/netstandard2.1/publish/wwwroot/_framework"
 const wasmDest = "./out/_framework";
+
+const tsConfig = "./tsconfig.json"
+const tsDest = "./out/"
+const tsTypings = "./typings";
 
 const wasmClean = () => {
 	return del(wasmDest, { force: true });
@@ -32,3 +39,23 @@ const wasmBuild = series(
 	wasmMove
 );
 exports["wasm-build"] = wasmBuild;
+
+const tsClean = parallel(
+	() => del(path.join(tsDest, "*.js"), { force: true }),
+	() => del(tsTypings, { force: true })
+);
+exports["ts-clean"] = tsClean;
+
+const tsBuild = series(
+	tsClean,
+	() => {
+		const tsProj = createProject(tsConfig);
+		const tsResult = tsProj.src().pipe(tsProj());
+
+		return merge(
+			tsResult.js.pipe(dest(tsDest)),
+			tsResult.dts.pipe(dest(tsTypings))
+		);
+	}
+);
+exports["ts-build"] = tsBuild;
