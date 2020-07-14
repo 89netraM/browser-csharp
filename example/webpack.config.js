9fc10a0a694c69e2dcec82198c8cba9f0399d2e1
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-14 17:10:20 +0200

    Example HTML and loading wasm

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: path.resolve(__dirname, "src/index.ts"),
@@ -14,6 +16,23 @@ module.exports = {
 	resolve: {
 		extensions: [ ".ts", ".js" ]
 	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					// The important bits. The _framework folder needs to be
+					// included as a static asset.
+					from: path.resolve(__dirname, "node_modules/browser-csharp/out/_framework"),
+					to: "_framework"
+				}
+			]
+		}),
+		new HtmlPlugin({
+			template: path.resolve(__dirname, "index.html"),
+			title: "Browser C# Example",
+			base: "/"
+		})
+	],
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist")
